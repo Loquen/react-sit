be4245d5e6cba476b6c85ed358821f2149d5d5be
Loquen <lrjones416@gmail.com>
2019-01-26 11:19:32 -0800

    added navbar responsively

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,13 @@ class Navbar extends Component {
 	render(){
 		return(
 			<div className="Navbar">
-				Sit!
+				<ul className="nav">
+					<li className="title">Sit!</li>
+					<li className="item">Time</li>
+					<li className="item">Set</li>
+					<li className="item">Track</li>
+				</ul>
+				
 			</div>
 		);	
 	}
